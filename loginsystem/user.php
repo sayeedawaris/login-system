7b026185c347305d223e27ec0cb81b1ea822d8fe
Sayeeda Waris <106420051+sayeedawaris@users.noreply.github.com>
2024-01-27 14:04:51 +0530

    Add files via upload
    Login system using PHP, MYSQL

---
+++ loginsystem/user.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  </head>
+  <body>
+  
+    <div class="container my-5">
+    <form method="post">
+  <div class="mb-3">
+    <label for="name" class="form-label">Name</label>
+    <input type="text" class="form-control" placeholder="Enter your name" name="name">
+  </div>
+  <div class="mb-3">
+    <label for="email" class="form-label">email</label>
+    <input type="text" class="form-control" placeholder="Enter your email" name="email">
+  </div>
+  <div class="mb-3">
+    <label for="name" class="form-label">Phone number</label>
+    <input type="text" class="form-control" placeholder="Enter your Phone number" name="name">
+  </div>
+  
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+    </div>
+  </body>
+</html>
